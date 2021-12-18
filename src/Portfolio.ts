299ff1d888d06518f517d708aeed63b7a223a8a1
Gasperan <gaspar.soto@gmail.com>
2021-12-17 22:27:24 -0300

    feat: add profit calculation

--- src/Portfolio.ts
+++ src/Portfolio.ts
@@ -1,16 +1,26 @@
 import { Stock } from "./Stock";
 
 class Portfolio {
-  private _stocks: Array<Stock>;
-
-  constructor() {}
+  private _stocks: Array<Stock> = new Array();
 
   get stocks(): Array<Stock> {
     return this._stocks;
   }
 
-  set stocks(stocks: Array<Stock>) {
-    this._stocks = stocks;
+  public addStock(stock: Stock) {
+    this._stocks.push(stock);
+  }
+
+  public getProfit(initialDate: Date, finalDate: Date): number {
+    const profitByStock = this._stocks.map((stock: Stock) => {
+        const priceDuringInitialDate: number = stock.getPrice(initialDate)
+        const priceDuringFinalDate: number = stock.getPrice(finalDate)
+
+        return priceDuringFinalDate - priceDuringInitialDate
+      })
+    console.log(profitByStock)
+
+    return profitByStock.reduce((previousProfit, nextProfit) => previousProfit + nextProfit, 0)
   }
 }
 
